2d7b0d3a30e8369164fa1b11f85a9e8be24ab34f
Shawn Chang <ebola777@yahoo.com.tw>
2017-07-09 16:18:49 +0800

    Add comments

--- soccer/environment.py
+++ soccer/environment.py
@@ -7,12 +7,18 @@ class Environment(metaclass=ABCMeta):
   """
   @abstractmethod
   def reset(self):
+    """Reset the environment and return the initial state.
+    """
     raise NotImplementedError()
 
   @abstractmethod
   def take_action(self, action):
+    """Take an action from the agent and return the observation.
+    """
     raise NotImplementedError()
 
   @abstractmethod
   def render(self):
+    """Render the environment.
+    """
     raise NotImplementedError()
