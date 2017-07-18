bfe0d6da3eb35f0e3920d2cc105f4f66dc573eec
Shawn Chang <ebola777@yahoo.com.tw>
2017-07-18 12:47:11 +0800

    Prepare to separate the map data

--- setup.py
+++ setup.py
@@ -89,9 +89,7 @@ setup(
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    package_data={
-        'pygame_soccer': ['data/**/*'],
-    },
+    package_data={},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
