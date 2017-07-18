a94ba1080ce6a416dff619978466fed10211c968
Shawn Chang <ebola777@yahoo.com.tw>
2017-07-18 13:44:14 +0800

    Bring back the resources in the package

---
+++ pygame_soccer/data/map/minecraft.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="minecraft" tilewidth="32" tileheight="32" tilecount="816" columns="24">
+ <image source="../tileset/minecraft_block_32x32.png" width="768" height="1088"/>
+</tileset>
