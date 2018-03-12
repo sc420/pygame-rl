007e595859ad5f26321e357060c5aba893929850
Shawn Chang <ebola777@yahoo.com.tw>
2018-03-12 17:52:56 +0800

    organize map data

--- pygame_rl/data/map/soccer/minecraft_tileset.tsx
+++ pygame_rl/data/map/soccer/minecraft_tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="minecraft_sprite" tilewidth="32" tileheight="32" tilecount="1104" columns="24">
- <image source="../tileset/minecraft_sprite_32x32.png" width="768" height="1472"/>
+ <image source="../../tileset/minecraft_sprite_32x32.png" width="768" height="1472"/>
 </tileset>
